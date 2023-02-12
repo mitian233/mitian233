d1ecffaad21eeab993f41f187c066bbb0361df4b
mitian233 <2304254165@qq.com>
2023-02-13 01:38:02 +0800

    Add Redirect

---
+++ vue-hp/src/components/countDown.vue
@@ -0,0 +1,41 @@
+<template>
+  <p>Or you will be redirected to <a :href="url">{{url}}</a> in {{countDownTime}}s.</p>
+</template>
+
+<script>
+let timer = null
+let countDownTime = 10
+export default {
+  name: "countDown",
+  props: {
+    url: {
+      type:String,
+      default: 'https://www.mikan.ac.cn'
+    },
+  },
+  data() {
+    return {
+      countDownTime: countDownTime,
+    };
+  },
+  mounted() {
+    timer = setInterval(() => {
+      countDownTime--
+      this.countDownTime = countDownTime
+      if (countDownTime === 0) {
+        clearInterval(timer)
+        window.open(this.url, '_self')
+      }
+    }, 1000);
+  },
+  unmounted() {
+    clearInterval(timer)
+    timer = null
+    countDownTime = 10
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-hp/src/views/NotFoundView.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="container">
+  <div class="item" style="text-align: center">
+    <h1>404</h1>
+    <h2>Page Not Found</h2>
+    <p>Sorry, the page you are looking for is temporarily unavailable on this site. </p>
+    <p>Go back to the <router-link to="/">homepage</router-link></p>
+    <count-down :url="routerPath" :key="new Date().getTime()" v-if="showCounter"/>
+  </div>
+</div>
+</template>
+
+<script>
+let _this = this
+import countDown from "@/components/countDown.vue";
+export default {
+  name: "NotFoundView",
+  components: {
+    countDown
+  },
+  data() {
+    return {
+      showCounter: true,
+      routerPath: 'https://www.mikan.ac.cn'+this.$route.path,
+    };
+  },
+  beforeRouteLeave(to, from, next) {
+    this.showCounter = false
+    next()
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
